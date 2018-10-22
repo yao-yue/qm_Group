f94115b074d571ec1012f8b835865f51879ab48c
longbits <2316570512@qq.com>
2018-10-22 22:57:54 +0800

    补充视频播放  全局数据接口概念

--- wxApp/app.js
+++ wxApp/app.js
@@ -1,39 +1,39 @@
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  // 生命周期函数   在页面开始的时候 
+  onLaunch() {
+    console.log('应用启动');
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5bcdcefa6c5cf5220c328d71/video/video#!method=get',
+      success: (response) => {
+        // 把数据塞到globaData里面,传统方法
+        // const {slides, vehicles, stories} = response.data;
+        // this.globalData.slides = slides;
+        // this.globalData.vehicles = vehicles;
+        // this.globalData.stories = stories;
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
+        // Object.assign() 方法用于将所有可枚举属性的值从一个或多个源对象复制到目标对象。它将返回目标对象。
+        Object.assign(this.globalData, response.data 
+        //   {
+        //   'moreResoure': '多个源对象都能复制到目标对象'
+        // }
+        );
+        console.log(this.globalData);
+      },
+      error: function(err) {
+        console.log(err);
       }
-    })
+    });
+
+
   },
+  // 全局的数据 App.js 是全局的，可以解决跨页面的授权问题 
+  // 用户的登录状态等
   globalData: {
-    userInfo: null
-  }
+    
+    motto: '孙悟空大闹天宫',
+    
+  },
+
+ 
 })
--- wxApp/pages/stories/index.js
+++ wxApp/pages/stories/index.js
@@ -2,6 +2,31 @@ const app = getApp()
 
 Page({
     data: {
+        stories: [],
+        // 播放视频
+        currentVid: null,
+        currentVideo: null
+    },
+    onLoad () {
+        this.setData({
+            stories: app.globalData.stories
+        })
+    },
 
+    play(event) {
+        if(this.data.currentVid !== null) {
+            this.data.currentVideo.pause();
+        }
+        console.log(event.currentTarget);
+        console.log(event.currentTarget.dataset);
+        const vid = event.currentTarget.dataset.vid;
+        const currentVideo = wx.createVideoContext(`${vid}`);
+        currentVideo.play();
+        this.setData({
+            currentVideo,
+            currentVid: vid
+        }, () => {
+            currentVideo.play()
+        })
     }
 });
