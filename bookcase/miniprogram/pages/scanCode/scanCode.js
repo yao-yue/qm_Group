f5ad6863b54d439f96c02d2bc16e5b383d5c04d8
longbits <2316570512@qq.com>
2018-10-24 21:13:41 +0800

    私家书柜小程序  初试云函数

---
+++ bookcase/miniprogram/pages/scanCode/scanCode.js
@@ -0,0 +1,32 @@
+Page({
+  scanCode: function() {
+    wx.scanCode({
+      onlyFromCamera: true,
+      scanType: ['barCode'],
+      success: res => {
+        console.log(res);
+        console.log(res.result);
+        wx.cloud.callFunction({
+          name: 'bookinfo',
+          data: {
+            isbn: res.result
+          },
+          success: res => {
+            console.log('=======================');
+            console.log(res);
+            const bookString = res.result;
+            const db = wx.cloud.database();
+            const book = db.collection('mybook');
+            db.collection('mybook').add({
+              data: JSON.parse(bookString)
+            }).then(res => {
+              console.log(res);
+            }).catch(err => {
+              console.log(err);
+            })
+          }
+        })
+      }
+    })
+  }
+})
