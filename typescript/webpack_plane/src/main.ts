1b140312e8c463c07669a99e7af7c154c62d11de
longbits <2316570512@qq.com>
2018-11-14 22:21:20 +0800

    egret+TS 飞机游戏

--- typescript/webpack_plane/src/main.ts
+++ typescript/webpack_plane/src/main.ts
@@ -1,3 +1,5 @@
+import { loadImage } from "./assetUtil";
+
 // egret Main 类 
 class Main extends egret.DisplayObjectContainer {
     constructor () {
@@ -6,11 +8,41 @@ class Main extends egret.DisplayObjectContainer {
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
     }
     onAddToStage (event) {
-        let textField = new egret.TextField();
-        this.addChild(textField);
-        textField.textColor = 0xff0000;
-        textField.text = "你好 世界！";
+        // let textField = new egret.TextField();
+        // this.addChild(textField);
+        // textField.textColor = 0xff0000;
+        // textField.text = "你好 世界！";
+        this.createGame();
     }
+    async createGame() {
+        // 加载资源 并行 Promise.All
+        const assets = [
+            'assets/background.png',
+            'assets/hero.png',
+            'assets/enemy.png',
+        ];
+        // loadImage  返回的是Promise
+        // assets =>  Promise[]
+       const bitmaps = await loadImage(assets) as egret.Bitmap[];
+       const [bg, hero, enemy] = bitmaps;
+       this.addChild(bg);
+       this.addChild(hero);
+       this.addChild(enemy); 
+       
+       this.centerAnchor(hero);
+       this.centerAnchor(enemy);
+
+       enemy.x = this.stage.stageWidth / 2;
+       enemy.y = 200;
+
+       hero.x = this.stage.stageWidth /2;
+       hero.y = this.stage.stageHeight -100;
+    }
+    centerAnchor (displayObject: egret.DisplayObject) {
+        displayObject.anchorOffsetX = displayObject.width / 2;
+        displayObject.anchorOffsetY = displayObject.height / 2;
+}
+
 } 
 window['Main'] = Main;
 egret.runEgret();
