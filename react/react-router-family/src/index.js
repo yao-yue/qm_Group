836a7c5c5b97ca44961e6a7fc4f42aeef04bae74
hbw-cloud <2316570512@qq.com>
2018-12-12 22:22:37 +0800

    css in js    进入伟大的js世界

--- react/react-router-family/src/index.js
+++ react/react-router-family/src/index.js
@@ -1,72 +1,96 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { TranstionGroup, CSSTransition } from 'react-transition-group'
+import { BrowserRouter as Router, Route, Link,Switch, Redirect } from 'react-router-dom'
 
-function BasicExample() {
+function AnimationExample() {
     return (
         <Router>
-            <div>
-                <ul>
-                    <li><Link to="/">Form</Link></li>  
+            <Route
+            render={({location}) => (
+            <div style={styles.fill}>
+                <Route exact path="/" render={() => <Redirect to="/hsl/10/90/50"/> } />
+                <ul style={styles.nav}>
+                    <NavLink to="/hsl/10/90/50">Red</NavLink>
+                    <NavLink to="/hsl/120/100/40">Green</NavLink>
+                    <NavLink to="/rgb/33/150/243">Blue</NavLink>
+                    <NavLink to="/rgb/240/98/246">Pink</NavLink>
                 </ul>
-                <ul>
-                    <li><Link to="/about">About</Link></li>
-                </ul>
-                <ul>
-                    <li><Link to="/topics">Topics</Link></li>
-                </ul>
-                <hr/>
-                <Route path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/topics" component={Topics} />
+                <div style={styles.content}>
+                    <Switch location={location}>
+                        <Route exact path="/hsl/:h/:s/:l" component={HSL} />
+                        <Route exact path="/rgb/:r/:g/:b" component={RGB} />
+                    </Switch>
+                </div>
             </div>
+            ) } 
+            />
         </Router>
     )
 }
-
-function Home() {
+function HSL({match: {params}}) {
     return (
-        <div>
-            <h2></h2>
+        <div style={{...styles.fill,...styles.hsl,background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`}}>
+            HSL({params.h}, {params.s}%, {params.l}%)
         </div>
+    );
+}
+function RGB({match: {params}}) {
+    return (
+        <div style={{...styles.fill,...styles.hsl,background: `rgb(${params.r}, ${params.g}, ${params.b})`}}>
+            RGB({params.r}, {params.g}, {params.b})
+        </div>
+    );
+}
+function NavLink(props) {
+    return(
+        <li style={styles.navItem}>
+            <Link {...props}/>
+        </li>
     )
 }
-
-function About() {
-    return (
-        <div>
-            <h2>About</h2>
-        </div>
-    )
+// 让css变成js的资源  可以复用
+const styles = {};
+styles.fill = {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+};
+styles.content = {
+    ...styles.fill,
+    top: '40px',
+    textAlign: 'center'
 }
-function Topics({ match }) {
-    return (
-        <div>
-            <h2>Topics</h2>
-            <ul>
-                <li>
-                    <Link to={ `${match.url}/rendering` }>Render with React</Link>
-                </li>
-                <li>
-                    <Link to={ `${match.url}/components` }>Components</Link>
-                </li>
-                <li>
-                    <Link to={ `${match.url}/props-v-state` }>Props State</Link>
-                </li>
-            </ul>
-            <Route path={`${match.url}/:topicId`}  component={Topic}/>
-            <Route exact path={match.url} render={() => <h3>Please select a tipic.</h3>}/>
-        </div>
-    )
+styles.nav = {
+    position: "absolute",
+    padding: 0,
+    margin: 0,
+    top: 0,
+    height: "40px",
+    width: "100%",
+    display: "flex",
 }
-
-function Topic({match}) {
-    return (
-        <div>
-            {match.params.topicId}
-        </div>
-    )
+styles.navItem = {
+    flex: 1,
+    textAlign: "center",
+    listStyleType: "none",
+    padding: "10px",
+    background: "yellow"
 }
-
-
-ReactDOM.render(<BasicExample />, document.getElementById('root'));
+styles.hsl = {
+    ...styles.fill,
+    color: "white",
+    paddingTop: "20px",
+    fontSize: "30px"
+  };
+  
+  styles.rgb = {
+    ...styles.fill,
+    color: "white",
+    paddingTop: "20px",
+    fontSize: "30px"
+  };
+  
+ReactDOM.render(<AnimationExample />, document.getElementById('root'))
