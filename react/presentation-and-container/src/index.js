5fa9aac4ee1a16a81baa8b09a4f29ea07da66b92
hbw-cloud <2316570512@qq.com>
2018-12-30 17:03:29 +0800

    深层理解Store

--- react/presentation-and-container/src/index.js
+++ react/presentation-and-container/src/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ClockContainer from './Clock/index.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
-ReactDOM.render(<ClockContainer time={new Date()} />, document.getElementById('root'));
+var Store = {
+    _flag: false,
+    _handlers: [],
+    subscribe: function(handler) {
+        this._handlers.push(handler);
+    },
+    set: function(value) {
+        this._flag = value;
+        this._handlers.forEach(handler => handler(value));
+    },
+    get: function() {
+        return this._flag;
+    }
+}
 
+// onChange 调用 Store的set(!value)
+function Switcher({value, onChange}) {
+    return (
+        <button onClick={e=> onChange(!value)}>     
+            {value? 'light on': 'light off'}
+        </button>
+    )
+}
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: Store.get()
+        }
+        Store.subscribe((value) => {
+            this.setState({value,})   //引起重新渲染的核心代码
+        })
+    }
+    render() {
+        return (
+            <div>
+                <Switcher value={this.state.value} onChange={Store.set.bind(Store)}></Switcher>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
