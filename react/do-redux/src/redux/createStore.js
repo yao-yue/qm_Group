37bdb1408f3d8151087401573cfd6b2b59337338
hbw-cloud <2316570512@qq.com>
2019-02-07 09:31:07 +0800

    注释

--- react/do-redux/src/redux/createStore.js
+++ react/do-redux/src/redux/createStore.js
@@ -1,6 +1,6 @@
 // 总开关   提供一个状态树 + 状态写
 export const createStore = (state, storeChange) => {
      const store = state || {};
-     const dispatch = (action) => storeChange(store, action);
+     const dispatch = (action) => storeChange(store, action);  //dispatch 分发action给Reducer
      return { store, dispatch}
 }
